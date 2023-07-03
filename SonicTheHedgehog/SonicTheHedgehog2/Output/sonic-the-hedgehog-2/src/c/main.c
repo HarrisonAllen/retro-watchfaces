2252d8fe9dbd6e3e047c74b817bd28768add69b5
Harrison Allen <hmallen@mit.edu>
2023-07-02 23:06:17 -0400

    Generated and modified sonic 2 watchface

--- SonicTheHedgehog/SonicTheHedgehog2/Output/sonic-the-hedgehog-2/src/c/main.c
+++ SonicTheHedgehog/SonicTheHedgehog2/Output/sonic-the-hedgehog-2/src/c/main.c
@@ -1,62 +1,19 @@
 #include <pebble.h>
 #include "resources/Backgrounds.h"
 #include "resources/Numbers.h"
-#include "resources/Palettes.h"
 #include "resources/Sprites.h"
+#include "resources/SpriteActor.h"
+#include "resources/Defines.h"
 #include "pebble-gbc-graphics-advanced/pebble-gbc-graphics-advanced.h"
-
-// TODO: Overhaul
-
-#define NUM_VRAMS 4
-#define NUM_BACKGROUNDS 3
-#define SCREEN_WIDTH 184
-#define SCREEN_HEIGHT 184
-#define SCREEN_X_OFFSET (SCREEN_WIDTH - PBL_DISPLAY_WIDTH) / 2
-#define SCREEN_Y_OFFSET (SCREEN_HEIGHT - PBL_DISPLAY_HEIGHT) / 2
-
-#define TIME_TILE_X 3
-#define TIME_TILE_Y 6
-#define TIME_Y_SHIFT 4
-#define TIME_LAYER 2
-#define NUMBER_HEIGHT 5
-#define NUMBER_WIDTH 3
-
-#define FRAME_DURATION 125 // In ms
 
 static Window *s_window;
 static GBC_Graphics *s_gbc_graphics;
+static AppTimer *s_frame_timer;
 
-static void load_tilesheet(uint32_t tilesheet_resource_id, uint8_t tilesheet_start_offset, uint8_t vram_start_offset, uint8_t vram_bank) {
-    // Calculate how many tiles are on the tilesheet
-    ResHandle handle = resource_get_handle(tilesheet_resource_id);
-    size_t res_size = resource_size(handle);
-    uint16_t tiles_to_load = res_size / GBC_TILE_NUM_BYTES;
+static uint8_t s_number_buffer[NUMBER_HEIGHT * NUMBER_WIDTH * TOTAL_NUMBERS * 2];
 
-    GBC_Graphics_load_from_tilesheet_into_vram(s_gbc_graphics, tilesheet_resource_id, tilesheet_start_offset, 
-                                                tiles_to_load, vram_start_offset, vram_bank);
-}
-
-static void load_background_palettes(uint8_t *palettes[]) {
-    for (uint8_t p = 0; p < GBC_PALETTE_NUM_PALETTES; p++) {
-        GBC_Graphics_set_bg_palette_array(s_gbc_graphics, p, palettes[p]);
-        GBC_Graphics_set_sprite_palette_array(s_gbc_graphics, p, SPRITE_PALETTES[p]);
-    }
-}
-
-static void load_sprite_palettes(uint8_t *palettes[]) {
-    for (uint8_t p = 0; p < GBC_PALETTE_NUM_PALETTES; p++) {
-        GBC_Graphics_set_sprite_palette_array(s_gbc_graphics, p, palettes[p]);
-    }
-}
-
-static void load_map(uint32_t map_resource_id, uint8_t *target_map) {
-    ResHandle handle = resource_get_handle(map_resource_id);
-    size_t res_size = resource_size(handle);
-    uint16_t tiles_to_load = res_size / GBC_TILE_NUM_BYTES;
-    resource_load_byte_range(handle, 0, target_map, tiles_to_load * GBC_TILE_NUM_BYTES);
-}
-
-static void load_tilemap(uint32 map_resource_id, )
+SpriteActor sprite_actors[NUM_SPRITE_ACTORS];
+static bool changing_sprites = false;
 
 static void draw_number(uint8_t background, uint8_t tile_x, uint8_t tile_y, char number) {
     uint8_t number_value;
@@ -67,10 +24,12 @@ static void draw_number(uint8_t background, uint8_t tile_x, uint8_t tile_y, char
     }
 
     uint8_t tile, attr;
+    int tile_index;
     for (uint8_t y = 0; y < NUMBER_HEIGHT; y++) {
         for (uint8_t x = 0; x < NUMBER_WIDTH; x++) {
-            tile = NUMBER_TILES[number_value][x + y * NUMBER_WIDTH];
-            attr = NUMBER_ATTRS[number_value][x + y * NUMBER_WIDTH];
+            tile_index = number_value * NUMBER_HEIGHT * NUMBER_WIDTH * 2 + x * 2 + y * NUMBER_WIDTH * 2;
+            tile = s_number_buffer[tile_index];
+            attr = s_number_buffer[tile_index + 1];
             GBC_Graphics_bg_set_tile_and_attrs(s_gbc_graphics, background, x + tile_x, y + tile_y, tile, attr);
         }
     }
@@ -85,83 +44,44 @@ static void load_time() {
     strftime(time_buffer, sizeof(time_buffer), clock_is_24h_style() ?
                                             "%H:%M" : "%I:%M", tick_time);
 
-    draw_number(TIME_LAYER, TIME_TILE_X + 0 * NUMBER_WIDTH + 0, TIME_TILE_Y, time_buffer[0]);
-    draw_number(TIME_LAYER, TIME_TILE_X + 1 * NUMBER_WIDTH + 1, TIME_TILE_Y, time_buffer[1]);
-    draw_number(TIME_LAYER, TIME_TILE_X + 2 * NUMBER_WIDTH + 1, TIME_TILE_Y, time_buffer[2]);
-    draw_number(TIME_LAYER, TIME_TILE_X + 3 * NUMBER_WIDTH + 1, TIME_TILE_Y, time_buffer[3]);
-    draw_number(TIME_LAYER, TIME_TILE_X + 4 * NUMBER_WIDTH + 2, TIME_TILE_Y, time_buffer[4]);
+    draw_number(TIME_LAYER, TIME_X_TILE_OFFSET + 0 * NUMBER_WIDTH + 0, TIME_Y_TILE_OFFSET, time_buffer[0]);
+    draw_number(TIME_LAYER, TIME_X_TILE_OFFSET + 1 * NUMBER_WIDTH + 1, TIME_Y_TILE_OFFSET, time_buffer[1]);
+    draw_number(TIME_LAYER, TIME_X_TILE_OFFSET + 2 * NUMBER_WIDTH + 1, TIME_Y_TILE_OFFSET, time_buffer[2]);
+    draw_number(TIME_LAYER, TIME_X_TILE_OFFSET + 3 * NUMBER_WIDTH + 1, TIME_Y_TILE_OFFSET, time_buffer[3]);
+    draw_number(TIME_LAYER, TIME_X_TILE_OFFSET + 4 * NUMBER_WIDTH + 2, TIME_Y_TILE_OFFSET, time_buffer[4]);
 }
 
 static void generate_backgrounds() {
-    // Load BG 0
-    load_map(RESOURCE_ID_DATA_BG_0_TILEMAP, s_gbc_graphics->bg_tilemaps + 0 * GBC_TILEMAP_NUM_BYTES);
-    load_map(RESOURCE_ID_DATA_BG_0_ATTRMAP, s_gbc_graphics->bg_attrmaps + 0 * GBC_ATTRMAP_NUM_BYTES);
-    GBC_Graphics_bg_set_scroll_pos(s_gbc_graphics, 0, SCREEN_X_OFFSET, SCREEN_Y_OFFSET);
+    (*LOAD_BACKGROUND_GROUP[rand() % NUM_BACKGROUND_GROUPS])(s_gbc_graphics, s_number_buffer);
 
-    // Load BG 1
-    load_map(RESOURCE_ID_DATA_BG_1_TILEMAP, s_gbc_graphics->bg_tilemaps + 1 * GBC_TILEMAP_NUM_BYTES);
-    load_map(RESOURCE_ID_DATA_BG_1_ATTRMAP, s_gbc_graphics->bg_attrmaps + 2 * GBC_ATTRMAP_NUM_BYTES);
-    GBC_Graphics_bg_set_scroll_pos(s_gbc_graphics, 1, SCREEN_X_OFFSET, SCREEN_Y_OFFSET);
+    GBC_Graphics_bg_set_scroll_pos(s_gbc_graphics, BG_LAYER, SCREEN_X_OFFSET, SCREEN_Y_OFFSET);
+    GBC_Graphics_bg_set_scroll_pos(s_gbc_graphics, FG_LAYER, SCREEN_X_OFFSET, SCREEN_Y_OFFSET);
 
-    // Set up BG 2
+    // Set up Time layer
     for (uint8_t y = 0; y < GBC_TILEMAP_HEIGHT; y++) {
         for (uint8_t x = 0; x < GBC_TILEMAP_WIDTH; x++) {
             GBC_Graphics_bg_set_tile_hidden(s_gbc_graphics, TIME_LAYER, x, y, true);
         }
     }
-    GBC_Graphics_bg_set_scroll_pos(s_gbc_graphics, TIME_LAYER, SCREEN_X_OFFSET, SCREEN_Y_OFFSET + TIME_Y_SHIFT);
+    GBC_Graphics_bg_set_scroll_pos(s_gbc_graphics, TIME_LAYER, SCREEN_X_OFFSET, SCREEN_Y_OFFSET + TIME_Y_PIXEL_SHIFT);
     load_time();
 }
 
-static void initialize_sprite_actor(SpriteActor *sprite_actor, uint8_t sprite_index) {
-    GRect bounds = GBC_Graphics_get_screen_bounds(s_gbc_graphics);
-
-    uint8_t sprite_num = ((rand() % TOTAL_SPRITES) / FRAMES_PER_SPRITE) * FRAMES_PER_SPRITE;
-    uint8_t *sprite_data = SPRITE_DATA[sprite_num];
-    sprite_actor->base_sprite = sprite_num;
-    sprite_actor->sprite_index = sprite_index;
-    sprite_actor->width = GBC_TILE_WIDTH << sprite_data[4];
-    sprite_actor->height = GBC_TILE_HEIGHT << sprite_data[5];
-    sprite_actor->center_x = (bounds.size.w / 2) - sprite_actor->width / 2 + GBC_SPRITE_OFFSET_X;
-    sprite_actor->x = GBC_SPRITE_OFFSET_X - sprite_actor->width;
-    sprite_actor->y = SPRITE_BOTTOM_Y - sprite_actor->height + GBC_SPRITE_OFFSET_Y - SCREEN_Y_OFFSET;
-    sprite_actor->state = AS_OFFSCREEN;
-}
-
-static void render_sprite_actor(SpriteActor *sprite_actor) {
-    uint8_t sprite_data_pos = sprite_actor->base_sprite + sprite_actor->walk_frame;
-    GBC_Graphics_oam_set_sprite(s_gbc_graphics,
-                                sprite_actor->sprite_index,
-                                sprite_actor->x,
-                                sprite_actor->y,
-                                SPRITE_DATA[sprite_data_pos][2],
-                                SPRITE_DATA[sprite_data_pos][3],
-                                SPRITE_DATA[sprite_data_pos][4],
-                                SPRITE_DATA[sprite_data_pos][5],
-                                0,
-                                0);
-    GBC_Graphics_oam_set_sprite_hidden(s_gbc_graphics, sprite_actor->sprite_index, sprite_actor->state == AS_OFFSCREEN);
-}
-
-static void render_sprites() {
-    for (uint8_t i = 0; i < NUM_SPRITE_ACTORS; i++) {
-        render_sprite_actor(&sprite_actors[i]);
-    }
-}
-
-static void sprite_actor_take_step(SpriteActor *sprite_actor) {
-    sprite_actor->walk_frame = sprite_actor->walk_frame + 1;
-    if (sprite_actor->walk_frame > WALK_FRAMES) {
-        sprite_actor->walk_frame = 1;
-    }
+static void load_new_sprite(SpriteActor *sprite_actor) {
+    (*LOAD_SPRITE_GROUP[rand() % NUM_SPRITE_GROUPS])(s_gbc_graphics,
+                                                     sprite_actor->vram_bank, 
+                                                     sprite_actor->vram_bank_offset,
+                                                     sprite_actor->palette, 
+                                                     &(sprite_actor->num_sprites), 
+                                                     &(sprite_actor->data));
 }
 
 static void step() {
     GRect bounds = GBC_Graphics_get_screen_bounds(s_gbc_graphics);
     if (!changing_sprites) {
         // Scroll backgrounds
-        GBC_Graphics_bg_move(s_gbc_graphics, 0, 1, 0);
-        GBC_Graphics_bg_move(s_gbc_graphics, 1, 2, 0);
+        GBC_Graphics_bg_move(s_gbc_graphics, BG_LAYER, BG_STEP_SIZE, 0);
+        GBC_Graphics_bg_move(s_gbc_graphics, FG_LAYER, FG_STEP_SIZE, 0);
     }
 
     // Update time
@@ -171,29 +91,29 @@ static void step() {
         for (uint8_t i = 0; i < NUM_SPRITE_ACTORS; i++) {
             if (sprite_actors[i].state == AS_WALK) {
                 sprite_actor_take_step(&sprite_actors[i]);
-                render_sprite_actor(&sprite_actors[i]);
+                sprite_actor_render(&sprite_actors[i], s_gbc_graphics);
             }
         }
     } else { // Move sprites
         for (uint8_t i = 0; i < NUM_SPRITE_ACTORS; i++) {
             if (sprite_actors[i].state == AS_WALK_ON) {
-                sprite_actors[i].x += 4;
+                sprite_actors[i].x += SPRITE_STEP_SIZE;
                 if (sprite_actors[i].x >= sprite_actors[i].center_x) {
                     sprite_actors[i].x = sprite_actors[i].center_x;
-                    sprite_actors[i].walk_frame = 0;
                     sprite_actors[i].state = AS_WALK;
                 } else {
                     sprite_actor_take_step(&sprite_actors[i]);
                 }
-                render_sprite_actor(&sprite_actors[i]);
+                sprite_actor_render(&sprite_actors[i], s_gbc_graphics);
             } else if (sprite_actors[i].state == AS_WALK_OFF) {
-                sprite_actors[i].x += 4;
+                sprite_actors[i].x += SPRITE_STEP_SIZE;
                 if (sprite_actors[i].x >= bounds.size.w + GBC_SPRITE_OFFSET_X) {
-                    initialize_sprite_actor(&sprite_actors[i], i);
+                    load_new_sprite(&sprite_actors[i]);
+                    sprite_actor_reset(&sprite_actors[i]);
                 } else {
                     sprite_actor_take_step(&sprite_actors[i]);
                 }
-                render_sprite_actor(&sprite_actors[i]);
+                sprite_actor_render(&sprite_actors[i], s_gbc_graphics);
             }
         }
     }
@@ -217,17 +137,39 @@ static void frame_timer_handle(void* context) {
     }
 }
 
+
 static void start_changing_sprites() {
     changing_sprites = true;
     s_frame_timer = app_timer_register(FRAME_DURATION, frame_timer_handle, NULL);
 }
 
-static void generate_sprites() {
+static void render_sprites() {
     for (uint8_t i = 0; i < NUM_SPRITE_ACTORS; i++) {
-        initialize_sprite_actor(&sprite_actors[i], i);
+        sprite_actor_render(&sprite_actors[i], s_gbc_graphics);
+    }
+}
+
+static void initialize_sprites() {
+    for (uint8_t i = 0; i < NUM_SPRITE_ACTORS; i++) {
+        uint8_t vram_bank = 1 + i;
+        uint8_t vram_start_offset = 0;
+        uint8_t palette_num = i;
+        uint8_t num_sprites;
+        const uint8_t *sprite_data;
+        (*LOAD_SPRITE_GROUP[rand() % NUM_SPRITE_GROUPS])(s_gbc_graphics, vram_bank, vram_start_offset, palette_num, &num_sprites, &sprite_data);
+
+        sprite_actor_init(&sprite_actors[i], 
+                        s_gbc_graphics,
+                        i,
+                        vram_bank,
+                        vram_start_offset,
+                        num_sprites,
+                        sprite_data,
+                        palette_num);
     }
 
     sprite_actors[0].state = AS_WALK_ON;
+    sprite_actors[0].hidden = false;
 
     render_sprites();
     start_changing_sprites();
@@ -235,10 +177,11 @@ static void generate_sprites() {
 
 static void time_handler(struct tm *tick_time, TimeUnits units_changed) {
     if (!changing_sprites) {
-        if (tick_time->tm_min == 0) {
+        if (UPDATE_EVERY_SECOND ? (tick_time->tm_sec == 0) : (tick_time->tm_min == 0)) {
             for (uint8_t i = 0; i < NUM_SPRITE_ACTORS; i++) {
                 if (sprite_actors[i].state == AS_OFFSCREEN) {
                     sprite_actors[i].state = AS_WALK_ON;
+                    sprite_actors[i].hidden = false;
                 } else if (sprite_actors[i].state == AS_WALK) {
                     sprite_actors[i].state = AS_WALK_OFF;
                 }
@@ -247,6 +190,9 @@ static void time_handler(struct tm *tick_time, TimeUnits units_changed) {
         }
         step();
     }
+    
+    // Update time
+    load_time();
 }
 
 static void will_focus_handler(bool in_focus) {
@@ -270,18 +216,17 @@ static void window_load(Window *window) {
 
     GBC_Graphics_set_screen_bounds(s_gbc_graphics, GBC_SCREEN_BOUNDS_FULL);
 
-    load_tilesheet(RESOURCE_ID_DATA_TILESHEET, 0, 0, 0);
-    create_palettes();
     generate_backgrounds();
+    GBC_Graphics_lcdc_set_bg_layer_enabled(s_gbc_graphics, MASK_LAYER, false);
     GBC_Graphics_lcdc_set_sprite_layer_z(s_gbc_graphics, NUM_BACKGROUNDS-1);
-    generate_sprites();
+    initialize_sprites();
 
     // Setup the frame timer that will call the game step function
-    // s_frame_timer = app_timer_register(FRAME_DURATION, frame_timer_handle, NULL);
+    s_frame_timer = app_timer_register(FRAME_DURATION, frame_timer_handle, NULL);
     app_focus_service_subscribe(will_focus_handler);
 
     // Setup time
-    tick_timer_service_subscribe(MINUTE_UNIT, time_handler);
+    tick_timer_service_subscribe(UPDATE_EVERY_SECOND ? SECOND_UNIT : MINUTE_UNIT, time_handler);
 
     // Display the graphics
     GBC_Graphics_render(s_gbc_graphics);
--- SonicTheHedgehog/SonicTheHedgehog2/Output/sonic-the-hedgehog-2/src/c/resources/Helpers.h
+++ SonicTheHedgehog/SonicTheHedgehog2/Output/sonic-the-hedgehog-2/src/c/resources/Helpers.h
@@ -3,7 +3,7 @@
 #include <pebble.h>
 #include "../pebble-gbc-graphics-advanced/pebble-gbc-graphics-advanced.h"
 
-static void load_tilesheet(GBC_Graphics *gbc_graphics, uint32_t tilesheet_resource_id, uint8_t tilesheet_start_offset, uint8_t vram_start_offset, uint8_t vram_bank) {
+static uint16_t load_tilesheet(GBC_Graphics *gbc_graphics, uint32_t tilesheet_resource_id, uint8_t tilesheet_start_offset, uint8_t vram_start_offset, uint8_t vram_bank) {
     // Calculate how many tiles are on the tilesheet
     ResHandle handle = resource_get_handle(tilesheet_resource_id);
     size_t res_size = resource_size(handle);
@@ -11,24 +11,25 @@ static void load_tilesheet(GBC_Graphics *gbc_graphics, uint32_t tilesheet_resour
 
     GBC_Graphics_load_from_tilesheet_into_vram(gbc_graphics, tilesheet_resource_id, tilesheet_start_offset, 
                                                 tiles_to_load, vram_start_offset, vram_bank);
+    return tiles_to_load;
 }
 
-static void load_map(GBC_Graphics, *gbc_graphics, uint32_t map_resource_id, uint8_t *target_map) {
+static void load_map(GBC_Graphics *gbc_graphics, uint32_t map_resource_id, uint8_t *target_map) {
     ResHandle handle = resource_get_handle(map_resource_id);
     size_t res_size = resource_size(handle);
     resource_load_byte_range(handle, 0, target_map, res_size);
 }
 
 static void load_tilemap(GBC_Graphics *gbc_graphics, uint32_t tilemap_resource_id, uint8_t bg_num) {
-    load_map(gbc_graphics, tilemap_resource_id, s_gbc_graphics->bg_tilemaps + bg_num * GBC_TILEMAP_NUM_BYTES);
+    load_map(gbc_graphics, tilemap_resource_id, gbc_graphics->bg_tilemaps + bg_num * GBC_TILEMAP_NUM_BYTES);
 }
 
 static void load_attrmap(GBC_Graphics *gbc_graphics, uint32_t attrmap_resource_id, uint8_t bg_num) {
-    load_map(gbc_graphics, attrmap_resource_id, s_gbc_graphics->bg_attrmaps + bg_num * GBC_TILEMAP_NUM_BYTES);
+    load_map(gbc_graphics, attrmap_resource_id, gbc_graphics->bg_attrmaps + bg_num * GBC_TILEMAP_NUM_BYTES);
 }
 
 static void load_resource(GBC_Graphics *gbc_graphics, uint32_t resource_id, uint8_t *target_buffer) {
     ResHandle handle = resource_get_handle(resource_id);
     size_t res_size = resource_size(handle);
-    resource_load(handle, target, res_size);
+    resource_load(handle, target_buffer, res_size);
 }
